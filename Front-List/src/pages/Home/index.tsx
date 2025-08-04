e2393837063807abc93316180b7ca33c37807d1b
Luiz <luizgustavoka66@gmail.com>
2025-08-03 22:28:06 -0300

    css home

--- Front-List/src/pages/Home/index.tsx
+++ Front-List/src/pages/Home/index.tsx
@@ -1,12 +1,56 @@
-// import { useState } from "react";
-// import reactLogo from "./assets/react.svg";
-// import viteLogo from "/vite.svg";
 import "./style.css";
+import {
+  IoCreateOutline,
+  IoCloseOutline,
+  IoPencilOutline,
+} from "react-icons/io5";
 
 function Home() {
+  const people = [
+    {
+      name: "Gabriel",
+    },
+    {
+      name: "Lucas",
+    },
+    {
+      name: "Luizao",
+    },
+  ];
+
   return (
-    <div>
-      <h1>Olá React</h1>
+    <div className="main-container">
+      <div className="home-container">
+        <div className="title-container">
+          <h1 className="title">Minha Lista</h1>
+          <IoCreateOutline id="icon" />
+        </div>
+        <div className="list-container">
+          <ul className="list">
+            <li className="list-item">
+              <span>1 Item</span>
+              <span>
+                <IoPencilOutline id="io-up" />
+                <IoCloseOutline id="io-del" />
+              </span>
+            </li>
+            <li className="list-item">
+              <span>2 Item</span>
+              <span>
+                <IoPencilOutline id="io-up" />
+                <IoCloseOutline id="io-del" />
+              </span>
+            </li>
+            <li className="list-item">
+              <span>3 Item</span>
+              <span>
+                <IoPencilOutline id="io-up" />
+                <IoCloseOutline id="io-del" />
+              </span>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
